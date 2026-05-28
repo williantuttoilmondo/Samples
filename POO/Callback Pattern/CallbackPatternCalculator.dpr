8b76c6713eaff2ceed3a02f4e75d41f0328cdfbf
Willian Tuttoilmondo <willian.lara@totvs.com.br>
2026-05-28 18:58:24 -0300

    Implementing clean architecture

--- POO/Callback Pattern/CallbackPatternCalculator.dpr
+++ POO/Callback Pattern/CallbackPatternCalculator.dpr
@@ -2,15 +2,14 @@ program CallbackPatternCalculator;
 
 uses
   Vcl.Forms,
-  Devspace.Classes.Operation in 'Operation\Devspace.Classes.Operation.pas',
-  Devspace.Classes.Operation.Basics in 'Operation\Devspace.Classes.Operation.Basics.pas',
-  Devspace.Classes.Operation.Factory in 'Operation\Devspace.Classes.Operation.Factory.pas',
-  Devspace.Classes.Operation.Power in 'Operation\Devspace.Classes.Operation.Power.pas',
-  Devspace.Consts.Operation in 'Operation\Devspace.Consts.Operation.pas',
+  Devspace.Classes.Operation in 'Contracts\Devspace.Classes.Operation.pas',
+  Devspace.Classes.Operation.Basics in 'Domain\Devspace.Classes.Operation.Basics.pas',
+  Devspace.Classes.Operation.Factory in 'Service\Devspace.Classes.Operation.Factory.pas',
+  Devspace.Classes.Operation.Power in 'Domain\Devspace.Classes.Operation.Power.pas',
   Devspace.Exceptions.Operation in 'Operation\Devspace.Exceptions.Operation.pas',
-  Devspace.Forms.Calculator in 'Calculator\Devspace.Forms.Calculator.pas' {Calculator},
+  Devspace.Forms.Calculator in 'Presentation\Devspace.Forms.Calculator.pas' {Calculator},
   Devspace.Helpers.TStrings in 'Helpers\Devspace.Helpers.TStrings.pas',
-  Devspace.Classes.Operation.Facade in 'Operation\Devspace.Classes.Operation.Facade.pas';
+  Devspace.Classes.Operation.Facade in 'Service\Devspace.Classes.Operation.Facade.pas';
 
 {$R *.res}
 
